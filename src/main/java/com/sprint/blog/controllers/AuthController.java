4b5c62f01b3993c5f16f73f5ffeb4bad80d63bca
AtharvaW03 <113792733+AtharvaW03@users.noreply.github.com>
2026-03-28 19:29:31 +0530

    Add authentication endpoint and JWT token service

---
+++ src/main/java/com/sprint/blog/controllers/AuthController.java
@@ -0,0 +1,34 @@
+package com.sprint.blog.controllers;
+
+import com.sprint.blog.domain.dtos.AuthResponse;
+import com.sprint.blog.domain.dtos.LoginRequest;
+import com.sprint.blog.services.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest){
+        UserDetails userDetails = authenticationService.authenticate(
+                loginRequest.getEmail(),
+                loginRequest.getPassword()
+        );
+        String tokenValue = authenticationService.generateToken(userDetails);
+
+        AuthResponse authResponse = AuthResponse.builder()
+                .token(tokenValue)
+                .expiresIn(86400)
+                .build();
+        return ResponseEntity.ok(authResponse);
+    }
+}
